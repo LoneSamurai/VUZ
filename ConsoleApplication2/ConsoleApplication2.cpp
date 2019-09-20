9ded186064159e40d943b0739ba9ea21f90291d9
LoneSamurai <55023087+LoneSamurai@users.noreply.github.com>
2019-09-20 21:21:45 +0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	cout << " Задание №1" << endl;
+	cout << " HELLO!!!!!!\n\n\n";
+	cout << " Задание №2" << endl;
+	cout << "\t    Имя: Даниил" << endl;
+	cout << "        Фамилия: Патика" << endl;
+	cout << "       Отчество: Евгениевич" << endl;
+	cout << "\t    Пол: мужской" << endl;
+	cout << "  Дата рождения: 21.01.2001" << endl;
+	cout << "\t  Адрес: Инженерная, 11, квартира 94" << endl;
+	cout << " Номер телефона: 095-362-0069" << endl;
+	cout << "    Место учёбы: ДНУ" << endl;
+	cout << "\t  Класс: ПА-19-1" << endl;
+	cout << "\t  Хобби: программирование" << endl;
+	cout << " \n\n\n Задание №3" << endl;
+	cout << " ПППППППП" << endl;
+	cout << " П      П" << endl;
+	cout << " П      П" << endl;
+	cout << " П      П" << endl;
+	cout << " П      П" << endl;
+	cout << " ПППППППП" << endl;
+
+	_getch();
+	return 0;
+}
