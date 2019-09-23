a69e1b0ca041e07cdfdb1d8a32c1ccc85206e6f2
LoneSamurai <Danhunter863@gmail.com>
2019-09-23 20:08:52 +0300

    Revert "Revert "Add files via upload""
    This reverts commit f7460f9b843693133fedcc7452a2895a8211f772.

---
+++ LW2 SWAP3/LW2 SWAP3/LW2 SWAP3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int a, b, c;
+	cout << "A=";
+	cin >> a;
+	cout << "B=";
+	cin >> b;
+	cout << "C=";
+	cin >> c;
+	int temp;
+	temp = b;
+	b = a;
+	a = c;
+	c = temp;
+	cout << "A= " << a << "\tB= " << b << "\tC= " << c;
+
+	_getch;
+	return 0;
+}
