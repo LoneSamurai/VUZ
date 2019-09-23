42b9f32fb0192a814c7660480564bd1c1b40c942
LoneSamurai <55023087+LoneSamurai@users.noreply.github.com>
2019-09-23 20:22:17 +0300

    Add files via upload

---
+++ LW2 SWAP3/LW2 SWAP3/LW2 SWAP3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int a, b, c;
+	cout << "A=";
+	cin >> a;
+	cout << "B=";
+	cin >> b;
+	cout << "C=";
+	cin >> c;
+	cout << "A= " << a << "\tB= " << b << "\tC= " << c << endl;
+	int temp;
+	temp = c;
+	c = b;
+	b = a;
+	a = temp;
+	cout << "A= " << a << "\tB= " << b << "\tC= " << c;
+
+	_getch;
+	return 0;
+}
